6c36cd5148b75cf5e923aae9ec76cebc3b84d91c
unknown <arya.araban21@gmail.com>
2022-11-16 11:58:24 +0330

    added conv theorem test

---
+++ ch4 - frequency/Butterworth_filter.m
@@ -0,0 +1,31 @@
+clear; 
+im = rgb2gray(imread('..\Images\4\Lena.bmp'));
+
+[m,n] = size(im);
+
+% creating a circle with euclidean distance. 
+% this circle is the same size as image and the center is 0 
+[x,y] = meshgrid(-(m/2):(m/2)-1, -(n/2):(n/2)-1);
+D = sqrt(x.^2+y.^2);
+
+
+n = 2; %the higher n is the closet it gets to ideal
+D0 = 30; 
+
+h = 1./(1 + (D./D0).^(2*n)); %the filter 
+
+% UNCOMMENT FOLLOWING LINE FOR HIGH PASS INSTEAD OF LOW PASS
+%h = 1 - h; 
+
+%fourier operations
+im_fft=fftshift(fft2(im));
+
+subplot(2,2,1); imshow(log(abs(im_fft)),[]);title('fourier');
+subplot(2,2,2); imshow(h);title('filter');
+
+out = im_fft .* h;
+subplot(2,2,3); imshow(log(abs(out)),[]);title('fft*h')
+
+out = uint8(ifft2(ifftshift(out)));
+subplot(2,2,4); imshow(uint8(out));title('result')
+
