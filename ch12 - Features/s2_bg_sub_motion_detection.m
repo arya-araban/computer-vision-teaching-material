6de1a03fb3129d10de1482d35f360fc385911d5f
unknown <arya.araban21@gmail.com>
2023-01-04 00:30:30 +0330

    minor change

--- ch12 - Features/s2_bg_sub_motion_detection.m
+++ ch12 - Features/s2_bg_sub_motion_detection.m
@@ -1,14 +1,15 @@
 clc, clear all, close all;
 
 vid_path = 'atrium.mp4';
+implay(vid_path);
 
 vid_obj = VideoReader(vid_path);
 vid = read(vid_obj); % Gives 4d video object
 
-refFrame = (vid(:,:,:,1));
+refFrame = (vid(:,:,:,1)); % Get first frame as reference
 imshow(refFrame);title('reference frame');
 
-implay(vid_path);
+
 
 totalNumFrames = vid_obj.NumberOfFrames;
 thresholdParam = 0.1;%how sensitive you want the model to be to changes in pixel values/luminosity.
@@ -20,14 +21,14 @@ writerObj.FrameRate = vid_obj.FrameRate;
 open(writerObj);
 
 refFrame = rgb2gray(refFrame);
+
 for i = (1:totalNumFrames)
     curFrame = (vid(:,:,:,i));
     curFrame_gray = rgb2gray(curFrame);
     diff_img = curFrame_gray - refFrame; % this is your 'c' matrix/variable
-    binarisedMask =  im2uint8(imbinarize(diff_img, thresholdParam));%This matrix has the value 1 wherever the curFrame
-                  %differs from refFrame and staisfies thresholdParam
-                  %constraint. Thus motion detection is captured by this
-                  %matrix
+    binarisedMask =  im2uint8(imbinarize(diff_img, thresholdParam));
+    %motion detection is captured by this matrix
+    
     imgt = horzcat(curFrame, repmat(binarisedMask,[1 1 3]));
     writeVideo(writerObj, imgt);
 end
